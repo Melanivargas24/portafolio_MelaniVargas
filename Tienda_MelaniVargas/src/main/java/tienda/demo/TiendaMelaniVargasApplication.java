0bcb8efdc7aba0530558c910bd388288ea0e200f
Melanivargas24 <mvargas30424@ufide.ac.cr>
2024-01-24 18:33:54 -0600

    Creación de proyecto

---
+++ Tienda_MelaniVargas/src/main/java/tienda/demo/TiendaMelaniVargasApplication.java
@@ -0,0 +1,13 @@
+package tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaMelaniVargasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaMelaniVargasApplication.class, args);
+	}
+
+}
